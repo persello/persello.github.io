80bcd56b709aaa7bc62db883db9a1ac79efbd1dc
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-05-11 14:31:15 +0200

    Test change for vercel.

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,7 @@
 import NavBar from '../components/navbar'
 import Layout from '../components/layout'
 
-export default function Home() {
+export default function ContactPage() {
   return (
     <Layout>
       <h1>Hello contact!</h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import NavBar from '../components/navbar'
 import Layout from '../components/layout'
 
-export default function Home() {
+export default function IndexPage() {
   return (
     <Layout>
       <h1>Hello index!</h1>
