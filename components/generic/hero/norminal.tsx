fb46fb8b5071cf2ae218665d81e59ad7d9e48141
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-05-12 19:48:10 +0200

    First public version?

--- components/generic/hero/norminal.tsx
+++ components/generic/hero/norminal.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import GitHubCard from '../generic/githubcard'
+import GitHubCard from '../githubcard'
 
 
 export default function NorminalHero() {
@@ -7,20 +7,26 @@ export default function NorminalHero() {
         <div className="sm:w-3/5 sm:pr-8">
             <div className="flex flex-col">
                 <h1 className="font-black text-6xl xl:text-8xl text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-yellow-200">Norminal</h1>
-                <h1 className="absolute filter blur-3xl opacity-60 font-black text-8xl text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-yellow-200">Norminal</h1>
+                <h1 className="absolute filter blur-3xl font-black text-6xl xl:text-8xl text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-yellow-200">Norminal</h1>
                 <p className="font-mono">
                     Check out Norminal, an app for <u>everything</u> about SpaceX. <br />
                  Norminal is still in development and you can try it out on your iPhone or iPad via <a href="https://testflight.apple.com/join/CirlA6HB" className="text-yellow-400 underline">TestFlight</a>.
-             </p>
+                </p>
 
-                <div className=" sm:visible">
+                <div className="hidden sm:block">
                     <GitHubCard repo="persello/norminal" />
                 </div>
             </div>
         </div>
 
-        <div className="self-center sm:w-2/5 pt-8">
+        <div className="self-center pt-8 sm:w-2/5">
             <Image src="/images/norminal1.png" width={400} height={800} layout="intrinsic" objectFit="contain" />
         </div>
+
+        <div className="visible sm:hidden">
+            <GitHubCard repo="persello/norminal" />
+        </div>
+
+
     </div>
 }
---
+++ components/generic/underconstruction.tsx
@@ -0,0 +1,8 @@
+export default function UnderConstruction() {
+    return <div className="flex flex-col items-center text-center p-12">
+        <svg className="h-20 w-20 text-gray-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+        </svg>
+        <h1 className="font-semibold text-3xl text-gray-400">Website under construction</h1>
+    </div>
+}
