874ee85d70b4f63bcbc81ac341b868fbc7be2dfe
Riccardo Persello <riccardo.persello+i@icloud.com>
2021-05-12 18:56:09 +0200

    Better GitHub card.

--- components/hero/norminal.tsx
+++ components/hero/norminal.tsx
@@ -4,11 +4,11 @@ import GitHubCard from '../generic/githubcard'
 
 export default function NorminalHero() {
     return <div className="flex flex-col sm:flex-row">
-        <div className="sm:w-3/5">
+        <div className="sm:w-3/5 sm:pr-8">
             <div className="flex flex-col">
                 <h1 className="font-black text-6xl xl:text-8xl text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-yellow-200">Norminal</h1>
                 <h1 className="absolute filter blur-3xl opacity-60 font-black text-8xl text-transparent bg-clip-text bg-gradient-to-br from-red-400 to-yellow-200">Norminal</h1>
-                <p className="font-mono pr-8">
+                <p className="font-mono">
                     Check out Norminal, an app for <u>everything</u> about SpaceX. <br />
                  Norminal is still in development and you can try it out on your iPhone or iPad via <a href="https://testflight.apple.com/join/CirlA6HB" className="text-yellow-400 underline">TestFlight</a>.
              </p>
